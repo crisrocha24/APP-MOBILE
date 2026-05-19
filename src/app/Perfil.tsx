710634bed2832fe58c359f80ac066abffe1fc852
crisrocha24 <christinarocha24@gmail.com>
2026-05-19 23:42:56 +0000

    comentario

---
+++ src/app/Perfil.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Perfil() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>
+            Perfil
+            </Text>
+        <Text style={styles.text}>
+            Ciência da computação
+        </Text>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/screens/Perfil.tsx
@@ -0,0 +1,6 @@
+import react from "react"
+import{
+    View,
+    Text,
+    StyleSheet,
+} from "react-native";
